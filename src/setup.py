5a66a2c2b8bcda90ec92f482336637eb445c91ac
Teresa Tamayo <ttamayo90@gmail.com>
2020-04-15 10:35:00 -0400

    Setting up python

---
+++ src/setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='orquestra',
+      package= setuptools.find_packages(where = "python"),
+      package_dir= {
+        "" : "python"
+     )
